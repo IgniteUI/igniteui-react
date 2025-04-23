67782166ea417d1563f161e6c437c5184f98d790
Radoslav Karaivanov <RKaraivanov@infragistics.com>
2025-04-23 12:17:45 +0000

    Merged PR 16181: refactor: `equal` helper function
    refactor: `equal` helper function
    
    Added support for not mindlessly blowing up the
    callstack when there are circular references.
    
    Fixed a stupid off-by-one error in the object
    key-value handling.
    
    Generated a new test-suite for the implementation.

--- src/equal.ts
+++ src/equal.ts
@@ -1,33 +1,33 @@
-export function equal<T>(a: unknown, b: T): boolean {
-  // Try the base case and return early
+export function equal<T>(a: unknown, b: T, visited = new WeakSet()): boolean {
+  // Early return
   if (Object.is(a, b)) return true;
 
-  // https://en.meming.world/wiki/File:Ah_Shit,_Here_We_Go_Again.jpg/
   if (isObject(a) && isObject(b)) {
     if (a.constructor !== b.constructor) return false;
 
-    let length: number;
-    let i: number;
-    let keys: string[];
-
-    // Arrays
-    if (Array.isArray(a) && Array.isArray(b)) {
-      length = a.length;
-      if (length !== b.length) return false;
-      for (i = length - 1; i !== 0; i--) {
-        if (!equal(a[i], b[i])) return false;
-      }
+    // Circular references
+    if (visited.has(a) && visited.has(b)) {
       return true;
     }
+
+    visited.add(a);
+    visited.add(b);
+
+    // RegExp
+    if (isRegExp(a) && isRegExp(b)) return a.source === b.source && a.flags === b.flags;
 
     // Maps
     if (a instanceof Map && b instanceof Map) {
       if (a.size !== b.size) return false;
-      for (const i of a.entries()) {
-        if (!b.has(i[0])) return false;
-      }
-      for (const i of a.entries()) {
-        if (!equal(i[1], b.get(i[0]))) return false;
+      for (const [keyA, valueA] of a.entries()) {
+        let found = false;
+        for (const [keyB, valueB] of b.entries()) {
+          if (equal(keyA, keyB, visited) && equal(valueA, valueB, visited)) {
+            found = true;
+            break;
+          }
+        }
+        if (!found) return false;
       }
       return true;
     }
@@ -35,31 +35,49 @@ export function equal<T>(a: unknown, b: T): boolean {
     // Sets
     if (a instanceof Set && b instanceof Set) {
       if (a.size !== b.size) return false;
-      for (const i of a.entries()) {
-        if (!b.has(i[0])) return false;
+      for (const valueA of a) {
+        let found = false;
+        for (const valueB of b) {
+          if (equal(valueA, valueB, visited)) {
+            found = true;
+            break;
+          }
+        }
+        if (!found) return false;
       }
       return true;
     }
 
-    // RegExp
-    if (isRegExp(a) && isRegExp(b)) return a.source === b.source && a.flags === b.flags;
+    // Arrays
+    if (Array.isArray(a) && Array.isArray(b)) {
+      const length = a.length;
+      if (length !== b.length) return false;
+      for (let i = 0; i < length; i++) {
+        if (!equal(a[i], b[i], visited)) return false;
+      }
+      return true;
+    }
+
     // toPrimitive
     if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
     // Strings based
     if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
 
-    keys = Object.keys(a);
-    length = keys.length;
-    if (length !== Object.keys(b).length) return false;
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+    if (aKeys.length !== bKeys.length) return false;
 
-    for (i = length - 1; i !== 0; i--) {
-      if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
+    for (const key of aKeys) {
+      if (!Object.prototype.hasOwnProperty.call(b, key)) return false;
     }
 
-    for (i = length - 1; i !== 0; i--) {
-      const key = keys[i];
-      if (!equal(a[key as keyof typeof a], b[key as keyof typeof b])) return false;
+    for (const key of aKeys) {
+      if (!equal(a[key as keyof typeof a], b[key as keyof typeof b], visited)) return false;
     }
+
+    visited.delete(a);
+    visited.delete(b);
+
     return true;
   }
 
