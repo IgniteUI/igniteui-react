59283fc9c28e39dd0a10d134821c74e574bb1753
damyanpetev <damyanpetev@users.noreply.github.com>
2026-01-07 18:42:43 +0200

    build(dock-manager): switch to default  build handling

--- scripts/build-dm.ts
+++ scripts/build-dm.ts
@@ -1,20 +1,8 @@
-import { mkdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { rimraf } from 'rimraf';
 import manifest from '../node_modules/igniteui-dockmanager/custom-elements.json';
 import pkg from '../node_modules/igniteui-dockmanager/package.json';
 import type { ClassField, Package } from './schema';
-import { getExports } from './typescript-utils';
-import {
-  type CustomElementWithPath,
-  createEvents,
-  createTypeExports,
-  formatSource,
-  getPackageJsonTypesEntry,
-  parseElementsJSON,
-  toReactName,
-} from './utils';
+import { type CustomElementWithPath, getPackageJsonTypesEntry, wrapWebComponents } from './utils';
 
 const pkgScope = process.env.IG_LICENSED_BUILD ? '@infragistics/' : '';
 const config = {
@@ -25,64 +13,36 @@ const config = {
   },
   types: {
     entry: join('node_modules/igniteui-dockmanager', getPackageJsonTypesEntry(pkg)),
-    // TODO: addResourceStrings temporary hidden until i18n story is defined for all
-    ignoreExports: new Set(['addResourceStrings']), // define's in /loader subpath, no need to ignore
+    ignoreExports: new Set([
+      // Now deprecated, so keeping previous ignore for React until removed
+      'addResourceStrings',
+    ]),
   },
-  ignore: new Set<string>(),
+  ignore: new Set([
+    // TODO: Cleanup custom-elements.json...
+    'igc-button-component',
+    'igc-context-menu',
+    'igc-icon-component',
+    'sample-component',
+    'igc-tab-header',
+    'igc-tab-panel',
+    'igc-tabs-component',
+    'igc-edge-docking-indicator',
+    'igc-joystick-icon',
+    'igc-joystick-indicator',
+    'igc-root-docking-indicator',
+    'igc-splitter-docking-indicator',
+    'igc-pane-navigator',
+    'igc-content-pane',
+    'igc-floating-pane',
+    'igc-pane-header',
+    'igc-resizer',
+    'igc-split-pane',
+    'igc-splitter',
+    'igc-unpinned-pane-header',
+  ]),
   ignoreEvents: new Set<string>(),
   templatesFilter: (_prop: ClassField, _declaration: CustomElementWithPath) => false,
 } as const;
 
-const dockManager = parseElementsJSON(manifest as Package).find(
-  (declaration) => declaration.tagName === 'igc-dockmanager',
-)!;
-
-const buffer: string[] = [
-  '/**',
-  ' * @module',
-  ' * @mergeModuleWith igniteui-react',
-  ' */',
-  "import * as React from 'react'",
-  `import { IgcDockManagerComponent as Component } from '${config.imports.default}'`,
-  `import type { IgcDockManagerComponentEventMap } from '${config.imports.types}'`,
-  "import { type EventName, createComponent } from '../react-props.js'",
-  '// HACK',
-  `import { defineCustomElements } from '${pkgScope}igniteui-dockmanager/loader'`,
-  'defineCustomElements()',
-];
-
-const root = fileURLToPath(new URL(config.path, import.meta.url));
-await rimraf(root);
-await mkdir(root, { recursive: true });
-
-const name = toReactName(dockManager.name);
-buffer.push(`
-/**
- * A Dock Manager component that provides means to manage the layout of your application through panes, allowing your end-users to customize it further by pinning, resizing, moving, maximizing and hiding panes.
- * @class
- */
-export const ${name} = createComponent({
-  tagName: '${dockManager.tagName}',
-  displayName: '${name}',
-  elementClass: Component,
-  react: React,
-  ${createEvents(dockManager, config)}
-});
-
-export type ${name} = Component;
-
-/** @deprecated Module register is no longer needed and can be removed */
-export const IgrDockManagerModule = { register: () => {} };
-`);
-
-const exports = await getExports(config.types.entry);
-const types = createTypeExports(
-  exports,
-  // should be dockManager.name, but that's listed as 'IgcDockManager'
-  ['IgcDockManagerComponent', ...config.types.ignoreExports],
-  config,
-);
-await writeFile(join(root, 'types.ts'), await formatSource(types), 'utf8');
-buffer.push(`export * from './types.js';`);
-
-await writeFile(join(root, 'index.ts'), await formatSource(buffer.join('\n')), 'utf8');
+await wrapWebComponents(manifest as Package, config);
